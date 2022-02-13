151d761231854a8eb7a179512f0d5702e8a12e50
Nathan Quinn <biomednquinn@gmail.com>
2022-02-13 17:21:06 +0900

    adds public/private sections

--- include/States.hpp
+++ include/States.hpp
@@ -5,12 +5,7 @@
 
 struct GameState
 {
-    struct Idle {};
-    struct Combat {};
-    struct Rest {};
-    struct SelfCheck {};
-    struct LookAround {};
-
+ public:
     using State = std::variant<Idle, Combat, Rest, SelfCheck, LookAround>;
 
     void StartFight() { state_ = std::visit(StartFightEvent(), state_); };
@@ -26,56 +21,63 @@ struct GameState
     void SelfCheck() { state_ = std::visit(SelfCheckEvent(), state_); };
     void FinishSelfCheck() { state_ = std::visit(ToIdleEvent(), state_); };
 
+ private:
+    struct Idle {};
+    struct Combat {};
+    struct Rest {};
+    struct SelfCheck {};
+    struct LookAround {};
+
     State state_;
-};
 
-struct ToIdleEvent
-{
-    State operator() (const Idle&) { return Idle{}; };
-    State operator() (const Combat&) { return Idle{}; };
-    State operator() (const Rest&) { return Idle{}; };
-    State operator() (const SelfCheck&) { return Idle{}; };
-    State operator() (const LookAround&) { return Idle{}; };
-};
+    struct ToIdleEvent
+    {
+        State operator() (const Idle&) { return Idle{}; };
+        State operator() (const Combat&) { return Idle{}; };
+        State operator() (const Rest&) { return Idle{}; };
+        State operator() (const SelfCheck&) { return Idle{}; };
+        State operator() (const LookAround&) { return Idle{}; };
+    };
 
-struct StartFightEvent
-{
-    State operator() (const Idle&) { return Combat{}; };
-    State operator() (const Combat&) { return Combat{}; };
+    struct StartFightEvent
+    {
+        State operator() (const Idle&) { return Combat{}; };
+        State operator() (const Combat&) { return Combat{}; };
 
-    State operator() (const Rest&) { return Rest{}; };
-    State operator() (const SelfCheck&) { return SelfCheck{}; };
-    State operator() (const LookAround&) { return LookAround{}; };
-};
+        State operator() (const Rest&) { return Rest{}; };
+        State operator() (const SelfCheck&) { return SelfCheck{}; };
+        State operator() (const LookAround&) { return LookAround{}; };
+    };
 
-struct TakeRestEvent
-{
-    State operator() (const Idle&) { return Rest{}; };
-    State operator() (const Combat&) { return Rest{}; };
-    State operator() (const Rest&) { return Rest{}; };
+    struct TakeRestEvent
+    {
+        State operator() (const Idle&) { return Rest{}; };
+        State operator() (const Combat&) { return Rest{}; };
+        State operator() (const Rest&) { return Rest{}; };
 
-    State operator() (const SelfCheck&) { return SelfCheck{}; };
-    State operator() (const LookAround&) { return LookAround{}; };
-};
+        State operator() (const SelfCheck&) { return SelfCheck{}; };
+        State operator() (const LookAround&) { return LookAround{}; };
+    };
 
-struct LookAroundEvent
-{
-    State operator() (const Idle&) { return LookAround{}; };
-    State operator() (const LookAround&) { return LookAround{}; };
+    struct LookAroundEvent
+    {
+        State operator() (const Idle&) { return LookAround{}; };
+        State operator() (const LookAround&) { return LookAround{}; };
 
-    State operator() (const Combat&) { return Combat{}; };
-    State operator() (const Rest&) { return Rest{}; };
-    State operator() (const SelfCheck&) { return SelfCheck{}; };
-};
+        State operator() (const Combat&) { return Combat{}; };
+        State operator() (const Rest&) { return Rest{}; };
+        State operator() (const SelfCheck&) { return SelfCheck{}; };
+    };
 
-struct SelfCheckEvent
-{
-    State operator() (const Idle&) { return SelfCheck{}; };
-    State operator() (const SelfCheck&) { return SelfCheck{}; };
+    struct SelfCheckEvent
+    {
+        State operator() (const Idle&) { return SelfCheck{}; };
+        State operator() (const SelfCheck&) { return SelfCheck{}; };
 
-    State operator() (const Combat&) { return Combat{}; };
-    State operator() (const Rest&) { return Rest{}; };
-    State operator() (const LookAround&) { return LookAround{}; };
+        State operator() (const Combat&) { return Combat{}; };
+        State operator() (const Rest&) { return Rest{}; };
+        State operator() (const LookAround&) { return LookAround{}; };
+    };
 };
 
 #endif // !COMBAT_CPP_STATE_HPP_
