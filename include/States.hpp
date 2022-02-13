896a3a7bb4a751b7557f897e4c810eae11961285
Nathan Quinn <biomednquinn@gmail.com>
2022-02-13 19:50:07 +0900

    refactors states for simplicity and arg passing

--- include/States.hpp
+++ include/States.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <variant>
+#include <optional>
 
 #include "Combatant.hpp"
 
@@ -12,16 +13,26 @@ struct GameState
     struct Idle {};
     struct Combat 
     {
-        Combat() : target(nullptr), player_initiative(0) {};
-        Combat(const Combat& t) : target(t.target), player_initiative(t.player_initiative) {};
+        Combat() : target(nullptr), target_key(), player_initiative(0) {};
+        Combat(Combatant* const target, const uint8_t initiative) 
+            : target(target)
+            , target_key()
+            , player_initiative(initiative)
+        {
+            target_key = target->GetName();
+            Utility::ConditionStringInPlace(target_key, true, true);
+        };
+        Combat(const Combat& t) : target(t.target), target_key(t.target_key), player_initiative(t.player_initiative) {};
         Combat& operator=(const Combat& t)
         {
             target = t.target;
+            target_key = t.target_key;
             player_initiative = t.player_initiative;
             return *this;
         }
 
         Combatant* target;
+        std::string target_key;
         uint8_t player_initiative;
     };
     struct Resting {};
@@ -36,70 +47,68 @@ struct GameState
 
     State GetState() { return state_; };
 
-    void StartFight() { state_ = std::visit(StartFightEvent(), state_); };
-    void FinishFight() { state_ = std::visit(ToIdleEvent(), state_); };
-    void Feinted() { state_ = std::visit(TakeRestEvent(), state_); };
+    void StartFight(Combatant* target, const uint8_t initiative) 
+    {
+        std::optional<State> new_state;
+        if (std::holds_alternative<Idle>(state_))
+        {
+            new_state = Combat{ target, initiative };
+        }
+        else
+        {
+            new_state = std::nullopt;
+        }
+        if (new_state.has_value()) { state_ = new_state.value(); };
+    };
+    void FinishFight() { state_ = Idle{}; };
 
-    void TakeRest() { state_ = std::visit(TakeRestEvent(), state_); };
-    void FinishRest() { state_ = std::visit(ToIdleEvent(), state_); };
+    void TakeRest() 
+    {
+        std::optional<State> new_state;
+        if (std::holds_alternative<Idle>(state_) || std::holds_alternative<Combat>(state_))
+        {
+            new_state = Resting{};
+        }
+        else
+        {
+            new_state = std::nullopt;
+        }
+        if (new_state.has_value()) { state_ = new_state.value(); };
+    };
+    void FinishRest() { state_ = Idle{}; };
 
-    void LookAround() { state_ = std::visit(LookAroundEvent(), state_); };
-    void FinishLook() { state_ = std::visit(ToIdleEvent(), state_); };
+    void LookAround() 
+    { 
+        std::optional<State> new_state;
+        if (std::holds_alternative<Idle>(state_))
+        {
+            new_state = LookingAround{};
+        }
+        else
+        {
+            new_state = std::nullopt;
+        }
+        if (new_state.has_value()) { state_ = new_state.value(); };
+    };
+    void FinishLook() { state_ = Idle{}; };
 
-    void SelfCheck() { state_ = std::visit(SelfCheckEvent(), state_); };
-    void FinishSelfCheck() { state_ = std::visit(ToIdleEvent(), state_); };
+    void SelfCheck()
+    {
+        std::optional<State> new_state;
+        if (std::holds_alternative<Idle>(state_))
+        {
+            new_state = SelfChecking{};
+        }
+        else
+        {
+            new_state = std::nullopt;
+        }
+        if (new_state.has_value()) { state_ = new_state.value(); };
+    };
+    void FinishSelfCheck() { state_ = Idle{}; };
 
  private:
     State state_;
-
-    struct ToIdleEvent
-    {
-        State operator() (const Idle&) { return Idle{}; };
-        State operator() (const Combat&) { return Idle{}; };
-        State operator() (const Resting&) { return Idle{}; };
-        State operator() (const SelfChecking&) { return Idle{}; };
-        State operator() (const LookingAround&) { return Idle{}; };
-    };
-
-    struct StartFightEvent
-    {
-        State operator() (const Idle&) { return Combat{}; };
-        State operator() (const Combat&) { return Combat{}; };
-
-        State operator() (const Resting&) { return Resting{}; };
-        State operator() (const SelfChecking&) { return SelfChecking{}; };
-        State operator() (const LookingAround&) { return LookingAround{}; };
-    };
-
-    struct TakeRestEvent
-    {
-        State operator() (const Idle&) { return Resting{}; };
-        State operator() (const Combat&) { return Resting{}; };
-        State operator() (const Resting&) { return Resting{}; };
-
-        State operator() (const SelfChecking&) { return SelfChecking{}; };
-        State operator() (const LookingAround&) { return LookingAround{}; };
-    };
-
-    struct LookAroundEvent
-    {
-        State operator() (const Idle&) { return LookingAround{}; };
-        State operator() (const LookingAround&) { return LookingAround{}; };
-
-        State operator() (const Combat&) { return Combat{}; };
-        State operator() (const Resting&) { return Resting{}; };
-        State operator() (const SelfChecking&) { return SelfChecking{}; };
-    };
-
-    struct SelfCheckEvent
-    {
-        State operator() (const Idle&) { return SelfChecking{}; };
-        State operator() (const SelfChecking&) { return SelfChecking{}; };
-
-        State operator() (const Combat&) { return Combat{}; };
-        State operator() (const Resting&) { return Resting{}; };
-        State operator() (const LookingAround&) { return LookingAround{}; };
-    };
 };
 
 #endif // !COMBAT_CPP_STATE_HPP_
