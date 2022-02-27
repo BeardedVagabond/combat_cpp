3ec0cbb156d6cd8f6e706157dc80167c25454039
Nathan Quinn <biomednquinn@gmail.com>
2022-02-27 16:30:45 +0900

    adds whitespacing for logical groups

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,16 @@ int main()
     std::cout << "Who will join the Combat...? |> ";
     std::string player_name;
     std::getline(std::cin, player_name);
+
     std::cout << "(Available classes: Barbarian, Bard, Cleric, Druid, "
         << "Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard)\n";
     std::cout << "NOTE: This affects your starting equipment, hit die, and max health!\n";
     std::cout << "What class do you fall under...? |>";
+
     std::string player_class;
     std::getline(std::cin, player_class);
     Utility::ConditionStringInPlace(player_class, true, true);
+
     std::cout << "I applaud your bravery, " << player_name << "\n\n";
  
     // Start main game loop (returns true when complete)
